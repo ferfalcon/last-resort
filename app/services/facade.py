eb9a0fdb4caedfbeace9024529ca78658a93a6a4
Fer <fernando.falcon@holbertonstudents.com>
2025-03-14 18:35:09 -0300

    User endpoint

--- app/services/facade.py
+++ app/services/facade.py
@@ -1,3 +1,4 @@
+from app.models.user import User
 from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
@@ -7,8 +8,29 @@ class HBnBFacade:
 		self.review_repo = InMemoryRepository()
 		self.amenity_repo = InMemoryRepository()
 
+	''' USER CRUD'''
 	def create_user(self, user_data):
-		pass
+		user = User(**user_data)
+		self.user_repo.add(user)
+		return user
+	
+	def get_user(self, user_id):
+		return self.user_repo.get(user_id)
+
+	def get_user_by_email(self, email):
+		return self.user_repo.get_by_attribute('email', email)
+	
+	def get_all_users(self):
+		return self.user_repo.get_all()
+	
+	def update_user(self, user_id, user_data):
+		user = self.user_repo.get(user_id)
+		if not user:
+			return None
+		user.update(user_data)
+		return user
+
+
 
 	def get_place(self, place_id):
 		pass
