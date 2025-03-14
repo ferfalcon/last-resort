203deef629a1a8cc379db1e62f559b572441f840
Fer <fernando.falcon@holbertonstudents.com>
2025-03-14 19:51:33 -0300

    Amenity endpoints

--- app/services/facade.py
+++ app/services/facade.py
@@ -1,14 +1,15 @@
 from app.models.user import User
+from app.models.amenity import Amenity
 from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
 	def __init__(self):
 		self.user_repo = InMemoryRepository()
+		self.amenity_repo = InMemoryRepository()
 		self.place_repo = InMemoryRepository()
 		self.review_repo = InMemoryRepository()
-		self.amenity_repo = InMemoryRepository()
 
-	''' USER CRUD'''
+	''' USER '''
 	def create_user(self, user_data):
 		user = User(**user_data)
 		self.user_repo.add(user)
@@ -29,6 +30,27 @@ class HBnBFacade:
 			return None
 		user.update(user_data)
 		return user
+
+	''' AMENITY '''
+	def create_amenity(self, amenity_data):
+		amenity = Amenity(**amenity_data)
+		self.amenity_repo.add(amenity)
+		return amenity
+	
+	def get_amenity(self, amenity_id):
+		return self.amenity_repo.get(amenity_id)
+	
+	def get_all_amenities(self):
+		return self.amenity_repo.get_all()
+
+	def update_amenity(self, amenity_id, amenity_data):
+		amenity = self.amenity_repo.get(amenity_id)
+		if not amenity:
+			return None
+		amenity.update(amenity_data)
+		return amenity
+
+
 
 
 
